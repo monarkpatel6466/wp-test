7284e52728135637f63e8bd358ab8bdd403e66cd
md054-Nitish <nitish.kaila@multidots.com>
2019-04-18 18:31:28 +0530

    change ignore list

--- wp-content/themes/twentynineteen/archive.php
+++ wp-content/themes/twentynineteen/archive.php
@@ -50,6 +50,7 @@ get_header();
 		</main><!-- #main -->
 	</section><!-- #primary -->
     <a class="<?php echo get_the_ID(); ?>">abcdf</a>
+    <a class="<?php echo get_the_ID(); ?>">abcdf</a>
 
 <?php
 get_footer();
