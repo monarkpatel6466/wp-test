abd40ed81c5dfef1f3b3b805b95977040dabf879
md054-Nitish <nitish.kaila@multidots.com>
2019-04-19 13:09:34 +0530

    remove composer

--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -6,6 +6,4 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
-    'NitishKaila\\WPVIPPreCommitHook\\' => array($vendorDir . '/nitishkaila/wp-vip-pre-commit-hook/src'),
-    'Dealerdirect\\Composer\\Plugin\\Installers\\PHPCodeSniffer\\' => array($vendorDir . '/dealerdirect/phpcodesniffer-composer-installer/src'),
 );
