e6faa5b33034caf40c71ee0508005f6a92b2c4c4
md054-Nitish <nitish.kaila@multidots.com>
2019-04-19 12:07:50 +0530

    added file

--- wp-content/index.php
+++ wp-content/index.php
@@ -8,7 +8,14 @@ $a = "adfsf";
 $a = "adfsf";
 $a = "adfsf";
 $a = "adfsf";
+$a = "adfsf";
+$a = "adfsf";
 ?>
+<a class="<?php echo $a; ?>">abc</a>
+<a class="<?php echo $a; ?>">abc</a>
+<a class="<?php echo $a; ?>">abc</a>
+<a class="<?php echo $a; ?>">abc</a>
+<a class="<?php echo $a; ?>">abc</a>
 <a class="<?php echo $a; ?>">abc</a>
 <a class="<?php echo $a; ?>">abc</a>
 <a class="<?php echo $a; ?>">abc</a>
