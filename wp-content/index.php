51d2ba3d2bf6ca7ad11a5008b7b1618b7af25ca4
md054-Nitish <nitish.kaila@multidots.com>
2019-04-19 11:59:38 +0530

    adfsdf

--- wp-content/index.php
+++ wp-content/index.php
@@ -6,7 +6,12 @@ $a = "adfsf";
 $a = "adfsf";
 $a = "adfsf";
 $a = "adfsf";
+$a = "adfsf";
+$a = "adfsf";
 ?>
+<a class="<?php echo $a; ?>">abc</a>
+<a class="<?php echo $a; ?>">abc</a>
+<a class="<?php echo $a; ?>">abc</a>
 <a class="<?php echo $a; ?>">abc</a>
 <a class="<?php echo $a; ?>">abc</a>
 <a class="<?php echo $a; ?>">abc</a>
